4dca9c09c073298e811f7df6df877e14de9dbe94
CullanP <cullanpage@gmail.com>
2021-06-06 05:59:44 -0400

    Optimize move scheduler

--- src/main/java/dev/esophose/playerparticles/particles/listener/PPlayerMovementListener.java
+++ src/main/java/dev/esophose/playerparticles/particles/listener/PPlayerMovementListener.java
@@ -3,7 +3,11 @@ package dev.esophose.playerparticles.particles.listener;
 import dev.esophose.playerparticles.PlayerParticles;
 import dev.esophose.playerparticles.manager.ConfigurationManager.Setting;
 import dev.esophose.playerparticles.manager.DataManager;
+import dev.esophose.playerparticles.objects.Pair;
 import dev.esophose.playerparticles.particles.PPlayer;
+import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -17,48 +21,56 @@ import org.bukkit.util.Vector;
 
 public class PPlayerMovementListener implements Listener {
     
-    private static final int CHECK_INTERVAL = 3;
-    private Map<UUID, Integer> timeSinceLastMovement;
-    private Map<UUID, Vector> previousVectors;
+    private static final int                                        CHECK_INTERVAL = 3;
+    private              Int2ObjectOpenHashMap<Pair<Integer, UUID>> timeSinceLastMovement;
+    private              Int2ObjectOpenHashMap<Vector>              previousVectors;
     
     public PPlayerMovementListener() {
         DataManager dataManager = PlayerParticles.getInstance().getManager(DataManager.class);
-        this.timeSinceLastMovement = new HashMap<>();
-        this.previousVectors = new HashMap<>();
+        this.timeSinceLastMovement = new Int2ObjectOpenHashMap<>();
+        this.previousVectors = new Int2ObjectOpenHashMap<>();
 
         Bukkit.getScheduler().runTaskTimer(PlayerParticles.getInstance(), () -> {
             for (Player player : Bukkit.getOnlinePlayers()) {
-                UUID playerUUID = player.getUniqueId();
-                Vector previousVector = this.previousVectors.get(playerUUID);
+                int      playerId        = player.getEntityId();
+                UUID     playerUUID      = player.getUniqueId();
                 Location currentLocation = player.getLocation();
-                Vector currentVector = new Vector(currentLocation.getBlockX(), currentLocation.getBlockY(), currentLocation.getBlockZ());
-                this.previousVectors.put(playerUUID, currentVector);
+                Vector   previousVector  = this.previousVectors.get(playerId);
+                Vector   currentVector   = new Vector(currentLocation.getBlockX(), currentLocation.getBlockY(), currentLocation.getBlockZ());
+
+                this.previousVectors.put(playerId, currentVector);
 
                 if (previousVector == null || !previousVector.equals(currentVector)) {
-                    if (!this.timeSinceLastMovement.containsKey(playerUUID)) {
-                        this.timeSinceLastMovement.put(playerUUID, 0);
+                    if (!this.timeSinceLastMovement.containsKey(playerId)) {
+                        this.timeSinceLastMovement.put(playerId, new Pair<>(0, playerUUID));
                     } else {
-                        this.timeSinceLastMovement.replace(playerUUID, 0);
+                        this.timeSinceLastMovement.replace(playerId, new Pair<>(0, playerUUID));
                     }
                 }
             }
 
-            List<UUID> toRemove = new ArrayList<>();
+            List<Integer> toRemove = new ArrayList<>();
 
-            for (UUID uuid : this.timeSinceLastMovement.keySet()) {
-                PPlayer pplayer = dataManager.getPPlayer(uuid);
+            for (Int2ObjectMap.Entry<Pair<Integer, UUID>> entryPair : this.timeSinceLastMovement.int2ObjectEntrySet()) {
+                Pair<Integer, UUID> pair     = entryPair.getValue();
+                int                 playerId = entryPair.getIntKey();
+                UUID                uuid     = pair.getValue();
+                PPlayer             pplayer  = dataManager.getPPlayer(uuid);
+
                 if (pplayer == null) {
-                    toRemove.add(uuid);
+                    toRemove.add(playerId);
                 } else {
-                    int standingTime = this.timeSinceLastMovement.get(uuid);
+                    int standingTime = pair.getKey();
+
                     pplayer.setMoving(standingTime < Setting.TOGGLE_ON_MOVE_DELAY.getInt());
+
                     if (pplayer.isMoving())
-                        this.timeSinceLastMovement.replace(uuid, standingTime + CHECK_INTERVAL);
+                        this.timeSinceLastMovement.replace(playerId, new Pair<>(standingTime + CHECK_INTERVAL, uuid));
                 }
             }
 
-            for (UUID uuid : toRemove)
-                this.timeSinceLastMovement.remove(uuid);
+            for (int playerId : toRemove)
+                this.timeSinceLastMovement.remove(playerId);
         }, 0, CHECK_INTERVAL);
     }
 
